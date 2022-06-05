e4c53894867484b762e4415a7ce5fb5d86af7aaf
Administrator <sshfc123@gmail.com>
2022-06-05 13:01:03 +0900

    2022.06.05

--- crawling2.py
+++ crawling2.py
@@ -1,12 +1,13 @@
 import urllib.request
-from bs4 import BeautifulSoup, Tag          # pip install bs4
+from bs4 import BeautifulSoup         # pip install bs4
 url = urllib.request.urlopen('https://www.naver.com/')
 html = url.read()
 문자열 = html.decode()
 
 # bs4로 자르기
-
 soup = BeautifulSoup(문자열, 'html.parser')
+# <title>을 찾기
 tag = soup.find('title')
+# 태그 지우기
 result= tag.text.strip()
 print(result)
